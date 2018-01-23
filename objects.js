d13e2b568633f50fba505c6a85d0a89752d39215
Mariam Tokarski <mariamtokarski@gmail.com>
2018-01-23 19:51:26 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,10 @@
-var playlist = {
+var playlist = new Object ({
   "neilYoung": "harvest moon",
   "brockhampton": "face",
   "aliciaKeys": "lesson learned",
   "goapele": "closer"
   "chopin": "nocturne in c minor"
-}
+})
 
 function updatePlaylist(playlist, artist, titles){
   return object.assign({}, playlist, {[artist]:titles})
