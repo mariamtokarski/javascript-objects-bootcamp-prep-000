df6de29e75a85344c7b9c0072374fe30e32b3851
Mariam Tokarski <mariamtokarski@gmail.com>
2018-01-23 19:33:59 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,10 @@
-var playlist = {neil young: "harvest moon", brockhampton: "face", alicia keys: "lesson learned", goapele: "closer"}
+var playlist = {neilYoung: "harvest moon", brockhampton: "face", aliciaKeys: "lesson learned", goapele: "closer"}
 
 function updatePlaylist(playlist, artist, titles){
   return object.assign({}, playlist, {[key-value]})
+}
+
+function removeFromPlaylist(playlist, artist){
+  delete playlist.aliciaKeys
+  return playlist
 }
